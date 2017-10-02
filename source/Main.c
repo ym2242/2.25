21e6a42722936b4179e825a206e5cc753fb9dd38
Yi-Ming You <t106360241@ntut.org.tw>
2017-10-02 20:51:33 +0800

    HW

---
+++ source/Main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("        Y\n       Y\nYYYYYYY\n       Y\n        Y\n\nMMMMMMMMM\n      MM\n    MM\n      MM\nMMMMMMMMM\n\n");
+	system("pause");
+	return 0;
+}
